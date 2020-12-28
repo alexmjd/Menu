5aa990121b3e00ecef5031b01fbc349ce99cc1ab
alexandremajda <majda.a@outlook.com>
2020-12-28 15:50:47 +0100

    [Feature] Display simple window.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Menu
+
+Classic training project about building a Cross Compiled Menu with SFML 2.5.
+
+The project is available for Windows and MacOs (Linux soon).
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Menu");
+
+    while (window.isOpen()) {
+        sf::Event event;
+
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed ||
+                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+//        window.draw();
+        window.display();
+    }
+
     return 0;
 }
